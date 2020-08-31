b040956b5787d94040eb1f4e5530f89bb8559b05
unknown <anacarolinaosantos85@gmail.com>
2020-08-31 22:17:09 +0300

    update'
    git commit -m upadate

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,8 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Product } from '../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+
+  constructor(
+    private ProductService: ProductService, 
+    private router: Router, 
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    //pegar o valor do atributo ID da url.
+    //http://localhost:4200/products/update/:id/:nome
+    const id =  this.route.snapshot.paramMap.get('id')
+
+    //buscar o produto via id no banco
+    this.ProductService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.ProductService.update(this.product).subscribe(() => {
+      this.ProductService.showMessage('Product updated successfully!')
+      this.router.navigate(['/products']);
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
