0664b0825beae12989c978559fe3852e29ed093e
unknown <anacarolinaosantos85@gmail.com>
2020-09-04 18:08:57 +0300

    delete cont

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,5 +1,8 @@
+import { ProductService } from './../product.service';
 import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { observable, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-delete',
@@ -9,18 +12,27 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDeleteComponent implements OnInit {
 
   product: Product
-  
-  constructor() { }
+  //consultando o backend para preencher o produto
+  constructor(private productService: ProductService,
+              private router: Router, 
+              private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activeRoute.snapshot.paramMap.get()
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
   }
 
-  DeleteProduct(): void {
-
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Product successfully deleted!')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
-
+    this.router.navigate(['/products'])
   }
 
 }
